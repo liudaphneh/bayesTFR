ea3f7f8f7421b2df61126e2b621e1fcd70c9269c
liudaphneh <stick.on.stars@gmail.com>
2024-05-20 11:02:34 -0400

    removing temp my.tfr.file fixes

--- R/wpp_data.R
+++ R/wpp_data.R
@@ -100,15 +100,6 @@ do.read.un.file <- function(un.file.name, wpp.year, my.file=NULL, present.year=2
 	if(!is.null(my.tfr.file)) {
 		if(verbose) cat('Reading file ', my.tfr.file, '.\n')
 		my.tfr_data <- read.tfr.file(file=my.tfr.file)
-		# DAPHNE TEMPORARY EDIT 20230903:
-		# read.tfr.file doesn't work with my file, so do this manually
-		my.tfr_data <- read.csv(file=my.tfr.file)
-		if(annual){
-		  colnames(my.tfr_data) <- c("country", "country_code", as.character(seq(1970, 2019)), "include_code")
-		} else{
-		  colnames(my.tfr_data) <- c("country", "country_code", paste0(seq(1970, 2015, by = 5), "-", seq(1975, 2020, by = 5)), "include_code")
-		}
-		# end Daphne
 		colnames(my.tfr_data)[colnames(my.tfr_data)=='name'] <- 'country'
 		if(use.wpp.data)
 		    cols.to.use <- colnames(my.tfr_data)[is.element(colnames(my.tfr_data), colnames(tfr_data))]
